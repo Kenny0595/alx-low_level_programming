6678e95099f8b94c0f17201d50d7a36b9f7829d9
kenny0595 <kennyyoung954@gmail.com>
2023-02-21 15:18:37 -0800

    my nloop commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,14 @@
 /**
  * print_alphabet - print all alphabelt in lowercase
  *
- * Return: 0
+ * Return: always 0;
  */
-
-
 
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a' ; letter <= 'z' ; letter++)
 		_putchar(letter);
-
 	_putchar('\n');
-	return (0);
 }
