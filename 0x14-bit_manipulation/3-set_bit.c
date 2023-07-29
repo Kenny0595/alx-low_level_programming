2fcc359d9185b8440265ac9c86e51f4fe1d94746
kenny0595 <kennyyoung954@gmail.com>
2023-07-29 03:27:53 -0700

    bit malnipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,9 +8,9 @@
  * wishing myself a goodluck
  * Return: 1 if  sucessful if otherwise -1
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index);
 {
-	if (index > 63)
+	if (index > 63);
 		return (-1);
 
 	*n = ((1UL << index) | *n);
